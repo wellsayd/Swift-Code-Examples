186ba5d07b902ddc2e9330599683e96f7476d5b1
bwells <bwells@CVB-564452.local>
2022-11-22 11:20:20 -0500

    Rating stars swiftui

---
+++ RatingStars.swift
@@ -0,0 +1,53 @@
+//
+//  RatingStars.swift
+//  VB ParkFinder
+//
+//  Created by bwells on 11/21/22.
+//
+
+import SwiftUI
+
+struct RatingStars: View {
+    
+    var rating: Double
+    var wholeNum: Int
+    var remainderNum: Double
+    
+    init(rating: Double, wholeNum: Int, remainderNum: Double){
+        self.rating = rating
+        self.wholeNum = wholeNum
+        self.remainderNum = remainderNum
+    }
+
+    var body: some View {
+        HStack()
+        {
+            Text(String(rating))
+            ForEach(0..<wholeNum, id: \.self) { i in
+                Image("star")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20.0, height: 20.0, alignment: .top)
+                    .foregroundColor(.yellow)
+            }
+            if(remainderNum > 0.75){
+                Image("star")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20.0, height: 20.0, alignment: .top)
+                    .foregroundColor(.yellow)
+            }
+            
+            if(remainderNum > 0.25 && remainderNum < 0.75) {
+                Image("star_half")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20.0, height: 20.0, alignment: .top)
+                    .foregroundColor(.yellow)
+            }
+        }
+    }
+}
+
+
+
